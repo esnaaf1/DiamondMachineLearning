15980d1300322809faac75fb3ec5463c0cadd9b9
Farshad Esnaashari <farshad.esnaashari@gmail.com>
2021-12-09 23:35:51 -0600

    Changed the scikit-learn version to 0.24.1

--- makemodel.py
+++ makemodel.py
@@ -11,8 +11,7 @@ diamonds_df.set_index('id', inplace = True)
 X = diamonds_df.drop('type', axis =1)
 
 # Create our target.
-y = diamonds_df['type']
-
+y = diamonds_df['type'].values
 
 # Split our data into training and testing
 from sklearn.model_selection import train_test_split
@@ -26,11 +25,21 @@ classifier = LogisticRegression(solver='lbfgs',
    random_state=1)
 
 # Train the data
-classifier.fit(X_train.values, y_train)
+classifier.fit(X_train, y_train)
+
+# Predict outcomes for test data set
+y_pred = classifier.predict(X_test)
+
+# Evaluate the model
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
 
 # Save (pickle) the model
 from pickle import dump, load
 dump(classifier, open('classifier.pkl', 'wb'))
 
-
-
+# Load the model.
+loaded_classifier = load(open('classifier.pkl', 'rb'))
+# Predict outcomes for test data set
+y_pred = loaded_classifier.predict(X_test)
+print(f"Prediction: {y_pred[:10]}, Actual: {y_test[:10]}")
